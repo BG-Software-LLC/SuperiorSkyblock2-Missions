46237b31b8194aa54ee4dc80f14eee043dbf36e1
OmerBenGera <omerthepro126@gmail.com>
2022-11-20 17:19:18 +0200

    Fixed errors in recent versions of Spigot with the custom timings

--- IslandMissions/src/main/java/com/bgsoftware/superiorskyblock/missions/island/timings/TimingsWrapper.java
+++ IslandMissions/src/main/java/com/bgsoftware/superiorskyblock/missions/island/timings/TimingsWrapper.java
@@ -2,9 +2,12 @@ package com.bgsoftware.superiorskyblock.missions.island.timings;
 
 import co.aikar.timings.Timing;
 import co.aikar.timings.Timings;
+import com.bgsoftware.common.reflection.ReflectMethod;
 import org.bukkit.plugin.Plugin;
 
 public class TimingsWrapper implements ITimings {
+
+    private static final ReflectMethod<Timing> START_TIMING = new ReflectMethod<>(Timing.class, "startTiming");
 
     private final Timing handle;
 
@@ -14,7 +17,11 @@ public class TimingsWrapper implements ITimings {
 
     @Override
     public void startTiming() {
-        this.handle.startTiming();
+        try {
+            this.handle.startTiming();
+        } catch (Throwable error) {
+            START_TIMING.invoke(this.handle);
+        }
     }
 
     @Override
