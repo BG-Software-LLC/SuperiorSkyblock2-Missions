5af7dd7af9defe7f5fd0f90d1d32aee03c2dc363
OmerBenGera <omerthepro126@gmail.com>
2024-06-21 19:56:04 +0300

    Fixed NPE when timings handler method does not exist

--- Missions/IslandMissions/src/main/java/com/bgsoftware/superiorskyblock/missions/island/timings/TimingsWrapper.java
+++ Missions/IslandMissions/src/main/java/com/bgsoftware/superiorskyblock/missions/island/timings/TimingsWrapper.java
@@ -6,39 +6,57 @@ import co.aikar.timings.TimingsManager;
 import com.bgsoftware.common.reflection.ReflectMethod;
 import org.bukkit.plugin.Plugin;
 
+import javax.annotation.Nullable;
+
 public class TimingsWrapper implements ITimings {
 
     private static final ReflectMethod<Timing> START_TIMING = new ReflectMethod<>(Timing.class, "startTiming");
 
+    @Nullable
     private final Timing handle;
 
     public static ITimings create(Plugin plugin, String name) {
         return Timings.isTimingsEnabled() ? new TimingsWrapper(plugin, name) : DummyTimings.INSTANCE;
     }
 
-    private static final ReflectMethod<Timing> TIMINGS_MANAGER_GET_HANDLER = new ReflectMethod<>(
+    private static final ReflectMethod<Timing> TIMINGS_MANAGER_GET_HANDLER_WITH_PROTECT = new ReflectMethod<>(
             TimingsManager.class, "getHandler", String.class, String.class, Timing.class, boolean.class);
+    private static final ReflectMethod<Timing> TIMINGS_MANAGER_GET_HANDLER = new ReflectMethod<>(
+            TimingsManager.class, "getHandler", String.class, String.class, Timing.class);
 
     private TimingsWrapper(Plugin plugin, String name) {
         String pluginName = plugin.getName();
-        Timing pluginHandler = TIMINGS_MANAGER_GET_HANDLER.invoke(null,
-                pluginName, "Combined Total", TimingsManager.PLUGIN_GROUP_HANDLER, false);
-        this.handle = TIMINGS_MANAGER_GET_HANDLER.invoke(null,
-                pluginName, name, pluginHandler, true);
+
+        if (TIMINGS_MANAGER_GET_HANDLER_WITH_PROTECT.isValid()) {
+            Timing pluginHandler = TIMINGS_MANAGER_GET_HANDLER_WITH_PROTECT.invoke(null,
+                    pluginName, "Combined Total", TimingsManager.PLUGIN_GROUP_HANDLER, false);
+            this.handle = TIMINGS_MANAGER_GET_HANDLER_WITH_PROTECT.invoke(null,
+                    pluginName, name, pluginHandler, true);
+        } else if (TIMINGS_MANAGER_GET_HANDLER.isValid()) {
+            Timing pluginHandler = TIMINGS_MANAGER_GET_HANDLER.invoke(null,
+                    pluginName, "Combined Total", TimingsManager.PLUGIN_GROUP_HANDLER, false);
+            this.handle = TIMINGS_MANAGER_GET_HANDLER.invoke(null,
+                    pluginName, name, pluginHandler, true);
+        } else {
+            this.handle = null;
+        }
     }
 
     @Override
     public void startTiming() {
-        try {
-            this.handle.startTiming();
-        } catch (Throwable error) {
-            START_TIMING.invoke(this.handle);
+        if (this.handle != null) {
+            try {
+                this.handle.startTiming();
+            } catch (Throwable error) {
+                START_TIMING.invoke(this.handle);
+            }
         }
     }
 
     @Override
     public void stopTiming() {
-        this.handle.stopTiming();
+        if (this.handle != null)
+            this.handle.stopTiming();
     }
 
 }
